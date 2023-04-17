a56003fdfbda4eecb384a63bb851e123a1386481
kolhe05 <you@example.com>
2023-04-17 16:04:16 +0530

    first

---
+++ daythree/daythree.js
@@ -0,0 +1,33 @@
+alert("Congratulations!")
+alert("Hello Gauri")
+alert(3+4)
+/*let num=15;
+  alert (num) */
+/*var num=15;
+    alert(num)
+num=12;
+    alert(num)
+/*const a=165;
+    alert(a)*/
+//assignment operator
+/*   a=10
+   a*=10
+   alert(a)
+   
+let x=2;
+    y=3;
+   alert(x*=y)
+var res=12==0;
+   alert(res)              
+*/
+/*var x=13;
+var y=12;
+    alert(x==y && x>y)
+if(x>y)
+    alert("Gauri")
+else
+    alert("Kolhe")
+
+alert(typeof(true));
+let age=prompt("What's your age?",100)
+alert("Your age is :"+age)*/
